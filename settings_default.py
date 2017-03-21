cf91d7e1a90adfa5de7344434fb05548e9a160ad
Cristina Luis <cristina.luis@mkonline.com>
2017-03-21 13:50:04 +0100

    settings_default

--- settings_default.py
+++ settings_default.py
@@ -1,5 +1,12 @@
+# Copy this file to settings.py and change the values there.
+
+# Sesnder's email server and account
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "yourusername@gmail.com"
-EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
+EMAIL_HOST_USER = "yourusername@gmail.com"
+
+# Use the single-app password provide by google if using gmail
+EMAIL_HOST_PASSWORD = 'yourpassword'
+
+# Who to send the updates to
 EMAIL_RECIPIENT = "spike@spikesemail.com"
